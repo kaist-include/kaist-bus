8f064fd5fc603eae230bb040a81a3d782a4e6e01
Joonyoung Yi <joonyoung@bablabs.com>
2018-01-21 23:02:06 +0900

    날짜 변경 기능

--- app/controllers/bus/__init__.py
+++ app/controllers/bus/__init__.py
@@ -3,15 +3,12 @@ from datetime import date
 
 from flask import Flask, render_template, redirect, url_for, abort
 from flask import Blueprint
+from flask import request
 
 from ...func import firebase_api
-from ...func.weekday import get_weekday
+from ...func.weekday import get_weekday, put_weekday
 
 blueprint = Blueprint('bus', __name__, url_prefix='/buses')
-
-
-def is_weekday():
-    return date.today().weekday() < 5
 
 
 @blueprint.route("")
@@ -33,3 +30,15 @@ def detail_api(bus_id):
         buses=firebase_api.bus_list_api(),
         bus=bus,
         weekday=get_weekday())
+
+
+@blueprint.route("/weekday")
+def weekday_api():
+    data = request.args
+
+    weekday = data.get('weekday', None, type=int)
+    if weekday is not None:
+        put_weekday(weekday)
+
+    next_url = data.get('next_url', None)
+    return redirect(next_url)
--- app/func/weekday.py
+++ app/func/weekday.py
@@ -1,20 +1,34 @@
 from datetime import datetime
 
-# from flask import session
+from flask import session
 
-# KEY_WEEKDAY = 'weekday'
-# KEY_WEEKDAY_FORCED_DATETIME = 'weekday_forced_datetime'
+KEY_WEEKDAY = 'weekday'
+KEY_WEEKDAY_FORCED_DATETIME = 'weekday_forced_datetime'
+
+
+def _get_weekday_from_session():
+    # 강제로 설정한 weekday는 1시간을 유지합니다.
+    forced_datetime = session.get(KEY_WEEKDAY_FORCED_DATETIME, None)
+    if forced_datetime:
+        return session.get(KEY_WEEKDAY, None)
+    # if forced_datetime and (datetime.now() - forced_datetime).minutes < 60:
+
+    return None
 
 
 def get_weekday():
     now = datetime.now()
-    if now.hour < 4: # 4시전이면 전날 것을 적용합니다.
-        return (now.weekday() + 6) % 7
-    else:
-        return now.weekday()
+    weekday = _get_weekday_from_session()
+    if weekday is not None:
+        return weekday
 
-    # forced_datetime = session.get(KEY_WEEKDAY_FORCED_DATETIME, None)
-    # if not forced_datetime or (datetime.now() - forced_datetime).hours > 0:
-    #     session[KEY_WEEKDAY_FORCED_DATETIME] = None
-    #     return datetime.now().weekday
-    # return session.get(KEY_WEEKDAY, 0)
+    weekday = now.weekday()
+    if now.hour < 4:  # 4시전이면 전날 것을 적용합니다.
+        return (weekday + 6) % 7
+    else:
+        return weekday
+
+
+def put_weekday(weekday):
+    session[KEY_WEEKDAY_FORCED_DATETIME] = datetime.now()
+    session[KEY_WEEKDAY] = weekday
