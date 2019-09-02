2dde540f872f09b246b57f1f18ba2a2cf0bf65f0
jaeryoungka <jaeryoung.ka@gmail.com>
2019-09-02 19:56:49 +0900

    update/190902_2

--- warningForForcedHolidays.js
+++ warningForForcedHolidays.js
@@ -14,7 +14,7 @@ if (isHolidays) {
     var warningSign = '<div class="container">';
     warningSign += '<div class="bd-callout bd-callout-danger">';
     warningSign += '<h4>추석 미운행 안내</h4>';
-    warningSign += '<p>현재 보시고 계시는 버스는 9월 12일과 9월 13일에는 추석 연휴로 운행하지 않습니다.</p>';
+    warningSign += '<p>현재 보시고 계시는 버스는 9월 12일과 9월 13일에는 추석 연휴 관계로 운행하지 않습니다.</p>';
     warningSign += '</div></div>';
 
     $(document).ready(function() {
