fa442a21511a81e6b2400b844fd11cede971d3ea
jaeryoungka <jaeryoung.ka@gmail.com>
2021-02-28 20:53:22 +0900

    20210228

--- liveTickHelper.js
+++ liveTickHelper.js
@@ -30,7 +30,7 @@ array1 = [];
 expressBus = [];
 setInterval(function() {
     var date = new Date();
-    var forcedWeekends = [10, 11, 12];
+    var forcedWeekends = [1];
     var isWeekends = false;
     var isWeekendsYesterday = false;
     var day = date.getDay();
