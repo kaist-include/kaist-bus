70fba7b564f08197d8a17617323f8d690ee1d9ff
Jaeryoung <Jaeryoung>
2022-10-02 22:22:46 +0900

    2022.10.2. update

--- liveTickHelper.js
+++ liveTickHelper.js
@@ -31,7 +31,7 @@ array1 = [];
 expressBus = [];
 setInterval(function() {
     var date = new Date();
-    var forcedWeekends = [9,10,11,12];
+    var forcedWeekends = [3];
     var isWeekends = false;
     var isWeekendsYesterday = false;
     var day = date.getDay();
--- warningForWeekdays.js
+++ warningForWeekdays.js
@@ -1,5 +1,5 @@
 var date = new Date();
-var forcedWeekends = [9,10,11,12];
+var forcedWeekends = [3];
 var isWeekends = false;
 var day = date.getDay();
 var isDiff = false;
