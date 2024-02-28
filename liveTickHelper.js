701ed6f388f845e9ebcd07ebe006909ee77d2f5b
Jaeryoung <Jaeryoung>
2024-02-29 01:17:11 +0900

    240301

--- liveTickHelper.js
+++ liveTickHelper.js
@@ -31,7 +31,7 @@ array1 = [];
 expressBus = [];
 setInterval(function() {
     var date = new Date();
-    var forcedWeekends = [9, 12];
+    var forcedWeekends = [1];
     var isWeekends = false;
     var isWeekendsYesterday = false;
     var day = date.getDay();
--- warningForWeekdays.js
+++ warningForWeekdays.js
@@ -1,5 +1,5 @@
 var date = new Date();
-var forcedWeekends = [9, 12];
+var forcedWeekends = [1];
 var isWeekends = false;
 var day = date.getDay();
 var isDiff = false;
