fe76b7547a441326bbf44e6a4e69a2f2afbc2660
Jaeryoung <Jaeryoung>
2024-11-03 18:40:29 +0900

    20241103 업데이트

--- liveTickHelper.js
+++ liveTickHelper.js
@@ -31,7 +31,7 @@ array1 = [];
 expressBus = [];
 setInterval(function() {
     var date = new Date();
-    var forcedWeekends = [1,3,9];
+    var forcedWeekends = [];
     var isWeekends = false;
     var isWeekendsYesterday = false;
     var day = date.getDay();
--- warningForWeekdays.js
+++ warningForWeekdays.js
@@ -1,5 +1,5 @@
 var date = new Date();
-var forcedWeekends = [1,3,9];
+var forcedWeekends = [];
 var isWeekends = false;
 var day = date.getDay();
 var isDiff = false;
